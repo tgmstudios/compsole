7c396e69b1a144cd5e9ee0026254574ff27d9427
TGMstudios <aiden@tgmstudios.net>
2025-04-16 14:34:22 -0400

    revert

--- ui/src/components/console/openstack/novnc.tsx
+++ ui/src/components/console/openstack/novnc.tsx
@@ -1,40 +1,8 @@
-import { useEffect, useRef } from 'react';
-
 export default function OpenStackNoVNC({ consoleUrl }: { consoleUrl: string }) {
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-  
-  // Process URL and force remote cursor parameter
-  const secureConsoleUrl = (() => {
-    const url = new URL(consoleUrl.replace(/^http:/i, 'https:'));
-    url.searchParams.set('UI.cursor', 'remote');
-    url.searchParams.set('UI.viewOnly', 'false');
-    return url.toString();
-  })();
-
-  useEffect(() => {
-    const iframe = iframeRef.current;
-    if (!iframe) return;
-
-    const handleMouseEnter = () => {
-      document.documentElement.style.cursor = 'none';
-    };
-
-    const handleMouseLeave = () => {
-      document.documentElement.style.cursor = 'default';
-    };
-
-    iframe.addEventListener('mouseenter', handleMouseEnter);
-    iframe.addEventListener('mouseleave', handleMouseLeave);
-
-    return () => {
-      iframe.removeEventListener('mouseenter', handleMouseEnter);
-      iframe.removeEventListener('mouseleave', handleMouseLeave);
-    };
-  }, []);
+  const secureConsoleUrl = consoleUrl.replace(/^http:\/\//i, "https://");
 
   return (
     <iframe
-      ref={iframeRef}
       id="console"
       title="console"
       src={secureConsoleUrl}
@@ -42,9 +10,7 @@ export default function OpenStackNoVNC({ consoleUrl }: { consoleUrl: string }) {
         width: '100%',
         flexGrow: '1',
         display: 'block',
-        cursor: 'none'
       }}
-      allowFullScreen
     />
-  );
+  )
 }
